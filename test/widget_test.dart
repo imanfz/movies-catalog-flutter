26d338e045efb99f705f62b9a75e479eb8f709ba
Iman Faizal <imanfz1103@gmail.com>
2022-02-08 09:29:37 +0700

    move main.dart to lib directory

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:movies_catalog/ui/main.dart';
+import 'package:movies_catalog/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
